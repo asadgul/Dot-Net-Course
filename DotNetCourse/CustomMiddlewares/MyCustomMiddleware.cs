d7ba03f0be326cbf76bdec76a70efe6e2d2b33b9
Asad Gul <asadgul015@gmail.com>
2024-11-23 17:18:59 +0500

    Committed Middlwares

---
+++ DotNetCourse/CustomMiddlewares/MyCustomMiddleware.cs
@@ -0,0 +1,12 @@
+﻿
+namespace DotNetCourse.CustomMiddlewares
+{
+    public class MyCustomMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("Second Middleware");
+            await next(context);
+        }
+    }
+}
