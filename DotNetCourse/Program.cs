1a635e6add115ecbd272897b092e7c2428525160
Asad Gul <asadgul015@gmail.com>
2024-11-24 11:21:33 +0500

    Routing Committed

--- DotNetCourse/Program.cs
+++ DotNetCourse/Program.cs
@@ -8,18 +8,41 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.UseRouting();
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+app.UseEndpoints(endpoints =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    endpoints.Map("map1", async (context) =>{
+     await  context.Response.WriteAsync("response from map1");
+        //map will work for all get put post delete
+    });
+    endpoints.Map("map2", async (context) => {
+        await context.Response.WriteAsync("response from map2");
+        //map will work for all get put post delete
 
-app.UseHttpsRedirection();
+    });
+    endpoints.MapGet("mapGet", async (context) => {
+        await context.Response.WriteAsync("response from Map Get");
+        //map will work for all get put post delete
 
-app.UseAuthorization();
-
-app.MapControllers();
-
+    }); endpoints.MapPost("mapPost", async (context) => {
+        await context.Response.WriteAsync("response from map Post");
+        //map will work for all get put post delete
+    });
+});
+app.Run(async (context) =>
+{
+    context.Response.WriteAsync("response from EndPoint");
+});
 app.Run();
